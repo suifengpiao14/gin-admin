6beb4ddc4de1478a3684998ba835dfa87d98ceb1
jesse <2912150071@qq.com>
2020-10-28 23:40:32 +0800

    调整参数

--- internal/app/schema/schema.go
+++ internal/app/schema/schema.go
@@ -39,15 +39,15 @@ type ListResult struct {
 // PaginationResult 分页查询结果
 type PaginationResult struct {
 	Total    int  `json:"total"`
-	Current  uint `json:"current"`
-	PageSize uint `json:"pageSize"`
+	Current  uint `json:"index"`
+	PageSize uint `json:"size"`
 }
 
 // PaginationParam 分页查询条件
 type PaginationParam struct {
 	Pagination bool `form:"-"`                                     // 是否使用分页查询
 	OnlyCount  bool `form:"-"`                                     // 是否仅查询count
-	Current    uint `form:"current,default=1"`                     // 当前页
+	Current    uint `form:"pageIndex,default=1"`                   // 当前页
 	PageSize   uint `form:"pageSize,default=10" binding:"max=100"` // 页大小
 }
 
